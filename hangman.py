352789015621dc84eef4e9a576d349c07721c336
Marek Stopka <keramakpots@gmail.com>
2016-11-03 10:47:51 +0100

    Word() is working

--- hangman.py
+++ hangman.py
@@ -1,12 +1,16 @@
 import random
+import sys
 def main():
     name = input("Welcome in Hangman game. Whats your name?:")
-    capitals = ("Amsterdam", "Andorra", "Amnkara", "Athens", "Baku", "Belfast", "Belgrade", "Berlin", "Bern", "Bratislava",
-    "Brussels","Bucharest", "Budapest", "Copenhagen", "Dublin","Helsinki", "Kiev", "Lisbon",
-    "Ljubljana", "London", "Luxembourg", "Madrid", "Minsk", "Monaco", "Moscow", "Nicosia","Oslo",
-    "Paris", "Podgorica","Prague", "Pristina", "Reykjavik", "Riga", "Rome", "San Marino", "Sarajevo",
-    "Skopje", "Sofia", "Stockholm", "Tallinn", "Tbilisi", "Tirana", "Vienna", "Vilnius",
-    "Warsaw", "Zagreb")
+    capitals = ("AMSTERDAM", "ANDORRA", "AMNKARA", "ATHENS", "BAKU", "BELFAST",
+                "BELGRADE", "BERLIN", "BERN", "BRATISLAVA","BRUSSELS",
+                "BUCHAREST", "BUDAPEST", "COPENHAGEN", "DUBLIN","HELSINKI",
+                "KIEV", "LISBON", "LJUBLJANA", "LONDON", "LUXEMBOURG", "MADRID",
+                "MINSK", "MONACO", "MOSCOW", "NICOSIA","OSLO", "PARIS",
+                "PODGORICA","PRAGUE", "PRISTINA", "REYKJAVIK", "RIGA", "ROME",
+                "SAN MARINO", "SARAJEVO","SKOPJE", "SOFIA", "STOCKHOLM",
+                "TALLINN", "TBILISI", "TIRANA", "VIENNA", "VILNIUS","WARSAW",
+                "ZAGREB")
     guess = random.choice(capitals)
     print(guess)
     print("Try to guess a european capital")
@@ -22,6 +26,12 @@ def word(guess):
     trying = input("What's the capital?")
     if trying == guess:
         print("Congratulation! You've guessed the capital.")
+        Over = input("Do you want to play again(yes for start over/no for exit)")
+        if over == 'yes' or over == 'Yes' or over == 'YES':
+            main()
+        else
+            sys.exit()
+
     else:
         lifes =-1
         print("Unfortunately", trying, "is not my secret capital")
