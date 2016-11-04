f828770ab40e6e8f27fc8dd3e060f9a15f9c6791
Marek Stopka <keramakpots@gmail.com>
2016-11-04 21:21:41 +0100

    For the first time every function works

--- to_do.py
+++ to_do.py
@@ -15,7 +15,7 @@ def doing():
         #writing in a file
         answer1 = input("Add an item:")
         with open('lista.txt', 'a') as infile:
-            infile.write('[ ]'+ answer1 + '\n')
+            infile.writelines('[ ]'+ answer1 + '\n')
             print ("You've added", answer1, "to to-do list.")
     elif question == 'mark':
         #marking finished items
@@ -34,21 +34,26 @@ def doing():
                 if numer == index + 1:
                     x = data[index].replace(" ","X",1)
                     data[index] = x
-                    print(data[index][3:],"is completed.")
+                    print(data[index][:-1],"is completed.")
         with open('lista.txt', 'w') as infile:
             data = infile.writelines(data)
             pass
     elif question == 'archive':
         print("All completed task has been delated")
-        with open('lista.txt', 'r+') as infile:
-            data = infile.readlines()
-            for new in data:
-                if data[0] == '[ ]':
-                    infile.writelines(new)
-        with open('lista.txt', 'w') as infile:
-            data = infile.writelines(new)
-            pass
+        new_list = []
+        data = open("lista.txt", 'r+')
+        data.read
+        print(data)
 
+        for line in data:
+            if "[X]" not in line:
+                new_list.append(line)
+                print(new_list)
+        print(new_list)
+        data.close()
+        with open('lista.txt', 'w') as infile:
+            data = infile.writelines(new_list)
+            pass
 
     elif question == "x":
         # exit with a letter "x"
