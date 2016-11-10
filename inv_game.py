85cbeb22acfb0a499c156e3091b4267d9227c615
Marek Stopka <keramakpots@gmail.com>
2016-11-10 16:49:57 +0100

    Without import option

--- inv_game.py
+++ inv_game.py
@@ -1,5 +1,6 @@
 import collections
 import sys
+import operator
 import csv
 
 
@@ -42,11 +43,10 @@ def add_to_inventory(inv, loot):
     inv = collections.Counter(inv)
     loot = collections.Counter(loot)
     inv = inv+loot
-    print(inv)
     return inv
 
 
-#still not sorted
+#still not sorting
 def print_table(inv):
     order = input("Select the way how your inventory should be display: ")
     items = str(sum(inv.values()))
@@ -58,18 +58,19 @@ def print_table(inv):
         print("-----------")
         print("Total number of items:", items)
     elif order == "count,desc":
-        sorted(inv.values())
+        sorted_inv = sorted(inv.items(), key=operator.itemgetter(1))
+        sorted_inv.reverse()
         print("Inventory:")
         print("-----------")
-        for key, value in inv.items():
+        for key, value in sorted_inv:
             print('{:<3}: {:<8}'.format(value, key))
         print("-----------")
         print("Total number of items:", items)
     elif order == "count,asc":
-        sorted(inv, key=inv.__getitem__)
+        sorted_inv = sorted(inv.items(), key=operator.itemgetter(1))
         print("Inventory:")
         print("-----------")
-        for key, value in inv.items():
+        for key, value in sorted_inv:
             print('{:<3}: {:<8}'.format(value, key))
         print("-----------")
         print("Total number of items:", items)
@@ -78,15 +79,11 @@ def print_table(inv):
 def import_inventory(inv):
     with open('import_inventory.csv') as download:
         imported = csv.reader(download)
-        loot = dict(imported)
-        return others
-        loot = collections.Counter(loot)
-        inv = collections.Counter(inv)
-        inv = inv+loot
+        loot2 = dict(imported)
+        inv = inv.append(loot2)
         return inv
 
-
-def export_inventory():
+def export_inventory(inv):
     with open('export_inventory.csv', 'w', newline='') as saved:
         exported = csv.writer(saved)
         for key, value in inv.items():
